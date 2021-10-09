5fceb20f73925a1d05062686e1f5674791722234
Moses Cosme <mosesmaxcosme@gmail.com>
2021-10-09 04:33:55 -0500

    add state to Email field

--- src/components/FormFields/Email.js
+++ src/components/FormFields/Email.js
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function EmailField({ field }) {
   const { id, formId, type, label, description, cssClass, isRequired, placeholder } = field;
   const htmlId = `field_${formId}_${id}`;
+  const [ email, setEmail ] = useState('')
+  
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    return setEmail({...email, [name]: value});
+  }
 
   return (
     <div className={`${cssClass}`.trim()}>
       <label htmlFor={htmlId}>{label}</label>
       <input
         type="email"
-        name={String(id)}
+        name={id}
         id={htmlId}
         placeholder={placeholder}
         required={isRequired}
+        value={email.email}
+        onChange={handleChange}
       />
     </div>
   );
