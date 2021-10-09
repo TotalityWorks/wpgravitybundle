76e196bf8114ed96de12d62f82b844d8727fff46
Moses Cosme <mosesmaxcosme@gmail.com>
2021-10-09 04:53:12 -0500

    add state to Select field

--- src/components/FormFields/Select.js
+++ src/components/FormFields/Select.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function SelectField({ field }) {
   const { id, formId, type, label, description, cssClass, isRequired, defaultValue, choices } = field;
   const htmlId = `field_${formId}_${id}`;
+  const defaultState = {
+    [id]: defaultValue
+  };
+  const [ select, setSelect ] = useState(defaultState);
+  
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    return setSelect({[name]: value});
+  }
 
   return (
     <div className={`${cssClass}`.trim()}>
@@ -11,6 +20,8 @@ export default function SelectField({ field }) {
         name={id}
         id={htmlId}
         required={isRequired}
+        value={select.select}
+        onChange={handleChange}
       >
         {choices?.map(choice =>
           <option 
