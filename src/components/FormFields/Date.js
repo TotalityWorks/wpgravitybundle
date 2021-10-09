3caa7f545b589669a9c9511c2ce44ca35698cb8d
Moses Cosme <mosesmaxcosme@gmail.com>
2021-10-09 04:56:52 -0500

    add state and moment to Date field

--- src/components/FormFields/Date.js
+++ src/components/FormFields/Date.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import moment from 'moment';
 
 export default function DateField({ field }) {
   const { id, formId, type, label, description, cssClass, isRequired, placeholder } = field;
   const htmlId = `field_${formId}_${id}`;
+  const [ date, setDate ] = useState('');
+  
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    const formattedValue = moment(value).format('DD MMMM, YYYY');
+    console.log(date)
+    return setDate({[name]: formattedValue});
+  }
 
   return (
     <div className={`${cssClass}`.trim()}>
@@ -13,6 +22,8 @@ export default function DateField({ field }) {
         id={htmlId}
         required={isRequired}
         placeholder={placeholder || ''}
+        value={date.date}
+        onChange={handleChange}
       />
     </div>
   );
