b5c47da2133cf7e0ededb45e76123932606d4946
Moses Maximino Cosme II <mosesmaxcosme@gmail.com>
2022-07-20 00:06:20 -0500

    feat(website field): add zod schema and error handling to Website field

--- src/components/FormFields/Website.tsx
+++ src/components/FormFields/Website.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react"
+import { z } from "zod"
 
 import { useFormContext } from "../../formContext"
 import updateFormState from "../../updateFormState"
@@ -8,13 +9,31 @@ export default function WebsiteField(props: any) {
   const { id, formId, label, cssClass, isRequired, placeholder, size } = field
   const htmlId = `field_${formId}_${id}`
   const [websiteValue, setWebsiteValue] = useState("")
+  const [validateWebsite, setValidateWebsite] = useState<{
+    success?: any
+    error?: any
+  }>({})
   const classes = `${(size && size.toLowerCase()) || ""} ${cssClass}`.trim()
   const { dispatch } = useFormContext()
+
+  const requiredWebsiteSchema =
+    isRequired && z.string().url().startsWith("https://").min(12)
+  const websiteSchema =
+    requiredWebsiteSchema || z.string().startsWith("https://").url()
 
   const handleChange = (event: any) => {
     const { value } = event.target
     setWebsiteValue(value)
     const newWebsiteValue = value
+    // check for empty field, and if not required it should not be parsed
+    if ((value === "" || null) && !isRequired) {
+      // zod would give an error if an empty string is parsed
+      // so in this case we will reset state to avoid the error message.
+      setValidateWebsite({})
+      return updateFormState(field, newWebsiteValue, dispatch)
+    }
+    const validatedValue = websiteSchema.safeParse(value)
+    setValidateWebsite(validatedValue)
     return updateFormState(field, newWebsiteValue, dispatch)
   }
 
@@ -30,6 +49,11 @@ export default function WebsiteField(props: any) {
         value={websiteValue}
         onChange={handleChange}
       />
+      {validateWebsite && validateWebsite.error && (
+        <p className="error-message">
+          {validateWebsite.error.issues[0].message}
+        </p>
+      )}
     </div>
   )
 }
