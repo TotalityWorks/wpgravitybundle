2febf708736a4ac12b9a3d0b63a48ddb99714100
Moses Maximino Cosme II <mosesmaxcosme@gmail.com>
2022-07-21 06:36:58 -0500

    feat(phone field): add zod schema and error handling to Phone field

--- src/components/FormFields/Phone.tsx
+++ src/components/FormFields/Phone.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react"
+import { z } from "zod"
 
 import { useFormContext } from "../../formContext"
 import updateFormState from "../../updateFormState"
@@ -19,13 +20,40 @@ export default function PhoneField(props: any) {
   } = field
   const htmlId = `field_${formId}_${id}`
   const [phoneValue, setPhoneValue] = useState("")
+  const [validatePhone, setValidatePhone] = useState<{
+    success?: any
+    error?: any
+  }>({})
   const classes = `${(size && size.toLowerCase()) || ""} ${cssClass}`.trim()
   const { dispatch } = useFormContext()
+
+  const phoneNumberRegex =
+    /^\+?(\d{1,3}\s?)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/
+  // The following phone number inputs would be valid:
+  // +(111)-222-3333
+  // 1112223333
+  // +11112223333
+  // +1 (111)222-3333
+  // +12 (111)-222-3333
+  // +123 (111)-222-3333
+  // (111)-222-3333
+  // 111-222-3333
+
+  const phoneSchema = z.string().regex(phoneNumberRegex)
 
   const handleChange = (event: any) => {
     const { value } = event.target
     setPhoneValue(value)
     const newPhoneValue = value
+    // check for empty field, and if not required it should not be parsed
+    if ((value === "" || null) && !isRequired) {
+      // zod would give an error if an empty string is parsed
+      // so in this case we will reset state to avoid the error message.
+      setValidatePhone({})
+      return updateFormState(field, newPhoneValue, dispatch)
+    }
+    const validatedValue = phoneSchema.safeParse(value)
+    setValidatePhone(validatedValue)
     return updateFormState(field, newPhoneValue, dispatch)
   }
 
@@ -41,6 +69,9 @@ export default function PhoneField(props: any) {
         value={phoneValue}
         onChange={handleChange}
       />
+      {validatePhone && validatePhone.error && (
+        <p className="error-message">{validatePhone.error.issues[0].message}</p>
+      )}
     </div>
   )
 }
