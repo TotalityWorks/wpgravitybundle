139083107b9260da2b7a995660642daf85fc9be3
Moses Maximino Cosme II <mosesmaxcosme@gmail.com>
2022-11-18 12:57:15 -0600

    chore(all): setup dev dependencies and upgrade packages

--- example/src/app.js
+++ example/src/app.js
@@ -1,43 +1,42 @@
-import React, { useState } from 'react'
-import { render } from 'react-dom'
-import GravityForm, { useGravityFormMutation } from '../../src'
+import React, { useState } from "react"
+import { createRoot } from "react-dom"
+import GravityForm, { useGravityFormMutation } from "../../src"
 
-import query from '../../src/data/query.json'
-const form = query.data.gravityFormsForm;
-const buttonClass = 'btn btn-primary';
+import query from "../../src/data/query.json"
+const form = query.data.gravityFormsForm
+const buttonClass = "btn btn-primary"
 
 const App = ({ form, buttonClass }) => {
-    const [data, setData] = useState();
-    
-    const handleSubmit = (values) => {
-        const formData = values;
-        return setData(formData)
-    }
+  const [data, setData] = useState()
 
-    const gravityFormMutation = useGravityFormMutation(form)
+  const handleSubmit = values => {
+    const formData = values
+    return setData(formData)
+  }
 
-    return (
+  const gravityFormMutation = useGravityFormMutation(form)
+
+  return (
+    <>
+      {data ? (
         <>
-            {data ? 
-                <>
-                    <p>Data Submitted</p>
-                    <pre>{JSON.stringify(data, null, 2)}</pre>
-                </> 
-            : 
-                <>
-                    <GravityForm 
-                        form={form} 
-                        onSubmit={handleSubmit}
-                        buttonClass={buttonClass} 
-                    />
-                </>
-            }
-            <pre>{gravityFormMutation}</pre>
+          <p>Data Submitted</p>
+          <pre>{JSON.stringify(data, null, 2)}</pre>
         </>
-    )
+      ) : (
+        <>
+          <GravityForm
+            form={form}
+            onSubmit={handleSubmit}
+            buttonClass={buttonClass}
+          />
+        </>
+      )}
+      <pre>{gravityFormMutation}</pre>
+    </>
+  )
 }
 
-render(
-    <App form={form} buttonClass={buttonClass} />,
-    document.getElementById('root')
-);
+const root = createRoot(document.getElementById("root"))
+
+root.render(<App form={form} buttonClass={buttonClass} />)
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import GravityForm from './components/GravityForm';
-import useGravityFormMutation from './hooks/useGravityFormMutation';
-
-export default GravityForm;
-export { useGravityFormMutation };
